6c01213d20764888130fe72756053d64f99756d5
Taiwo Adedayo <taiwoadedayo@gmail.com>
2025-12-14 19:15:47 +0000

    Force ForgotPassword page to be a TS module

--- app/ecme/auth/ForgotPassword/page.tsx
+++ app/ecme/auth/ForgotPassword/page.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import ForgotPassword from './ForgotPassword'
 
 export default function ForgotPasswordPage() {
     return <ForgotPassword signInUrl="/ecme/auth/SignIn" />
 }
+
+export {}

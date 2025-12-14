518ff52894d9227cd649c7db9b7d2a12ed2aebba
Taiwo Adedayo <taiwoadedayo@gmail.com>
2025-12-14 18:56:58 +0000

    Remove accidental auth pages under components

--- app/ecme/components/auth/ResetPassword/page.tsx
+++
@@ -1,7 +0,0 @@
-import ResetPassword from './ResetPassword'
-
-const ResetPasswordPage = () => {
-  return <ResetPassword signInUrl="/ecme/auth/SignIn" />
-}
-
-export default ResetPasswordPage
---
+++ app/ecme/components/configs/theme.config.ts
@@ -0,0 +1,12 @@
+import type { Theme } from '../@types/theme'
+
+export const themeConfig = {
+  mode: 'light',
+  themeSchema: 'default',
+  direction: 'ltr',
+  panelExpand: false,
+  layout: {
+    type: 'modern',
+    sideNavCollapse: false,
+  },
+} as Theme

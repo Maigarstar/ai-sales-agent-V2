2147ac54268bc44b039bffe8380869a21696b086
Taiwo Adedayo <taiwoadedayo@gmail.com>
2025-12-14 21:37:37 +0000

    Add missing layout constants in theme.constant

--- app/ecme/constants/theme.constant.ts
+++ app/ecme/constants/theme.constant.ts
@@ -1,2 +1,9 @@
 export const MODE_LIGHT = 'light' as const
 export const MODE_DARK = 'dark' as const
+
+export const LAYOUT_BLANK = 'blank' as const
+export const LAYOUT_MODERN = 'modern' as const
+export const LAYOUT_CLASSIC = 'classic' as const
+export const LAYOUT_STACKED_SIDE = 'stackedSide' as const
+export const LAYOUT_TOP_BAR_CLASSIC = 'topBarClassic' as const
+export const LAYOUT_FRAMED_SIDE = 'framedSide' as const

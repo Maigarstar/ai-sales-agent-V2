43549cd4f982a3e32dd5c1a6686ce77446ecec28
Taiwo Adedayo <taiwoadedayo@gmail.com>
2025-12-14 22:45:15 +0000

    Add DIR_RTL and clean theme constants

--- app/ecme/constants/theme.constant.ts
+++ app/ecme/constants/theme.constant.ts
@@ -1,6 +1,9 @@
 export const MODE_LIGHT = 'light' as const
 export const MODE_DARK = 'dark' as const
 
+export const DIR_LTR = 'ltr' as const
+export const DIR_RTL = 'rtl' as const
+
 export const LAYOUT_BLANK = 'blank' as const
 export const LAYOUT_MODERN = 'modern' as const
 export const LAYOUT_CLASSIC = 'classic' as const
@@ -10,4 +13,5 @@ export const LAYOUT_FRAMED_SIDE = 'framedSide' as const
 export const LAYOUT_COLLAPSIBLE_SIDE = 'collapsibleSide' as const
 export const LAYOUT_CONTENT_OVERLAY = 'contentOverlay' as const
 
+// alias to keep older imports working
 export const LAYOUT_FRAMELESS_SIDE = LAYOUT_FRAMED_SIDE
